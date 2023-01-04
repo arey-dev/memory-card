f5e964c17428d4587223f32d9d6c153076b642ff
R.A Borja <raborja.dev@gmail.com>
2023-01-04 12:24:14 +0800

    Add Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+function Header() {
+  return (
+    <Header>
+      <div className="container">
+        <h1>Heroes Memory Card Game</h1>
+        <p>Get points by clicking on an image only once!</p>
+      </div>
+    </Header>
+  )
+}
+
+export default Header;
