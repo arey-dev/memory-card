0671431b400b4a9ae461abe21b7458d39dc508a7
R.A Borja <raborja.dev@gmail.com>
2023-01-05 16:14:31 +0800

    add styles to Card & CardsGrid component

--- src/GameBoard/CardsGrid/Card/Card.jsx
+++ src/GameBoard/CardsGrid/Card/Card.jsx
@@ -2,9 +2,9 @@ export function Card(props) {
   const { src, name } = props;
 
   return (
-    <div className="card flex">
+    <div className="card flex typography-card">
       <img src={src} alt={name} />
       <p>{name}</p>
     </div>
-  )
+  );
 }
--- src/GameBoard/CardsGrid/CardsGrid.jsx
+++ src/GameBoard/CardsGrid/CardsGrid.jsx
@@ -1,6 +1,8 @@
+import { Card } from "./Card/Card";
+
 export function CardsGrid(props) {
   return (
     <div className="container container--cards grid">
     </div>
-  )
+  );
 }
