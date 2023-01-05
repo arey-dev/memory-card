7cb80a9ea5c2b9d1855b56eb6f59d805b3cc7af4
R.A Borja <raborja.dev@gmail.com>
2023-01-05 14:43:31 +0800

    add styles to scoreboard.jsx

--- src/GameBoard/ScoreBoard.jsx
+++ src/GameBoard/ScoreBoard.jsx
@@ -1,8 +1,8 @@
 export function ScoreBoard(props) {
   return (
-    <div className="ScoreBoard">
-      <span>Current: </span>
-      <span>Best: </span>
+    <div className="scoreboard typography-scoreboard">
+      <span>Current Score: 50</span>
+      <span>Best Score: 12</span>
     </div>
   )
 }
